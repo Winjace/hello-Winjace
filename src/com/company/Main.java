bbb59ec9c1b68f95dcb77281e988129a8ac0643d
WInjace <15811599533@163.com>
2019-05-09 21:20:54 +0800

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("hello,Winjace");
+        int a = 1;
+        System.out.print(a);
     }
 }
